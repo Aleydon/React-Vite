e68587c73492c3f44c50581f80f9eec3b5a71267
Aleydon <aleydonroberto@gmail.com>
2025-01-29 02:32:24 -0300

    UP: Setup home router

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router';
+import { createBrowserRouter, RouterProvider } from 'react-router';
 
 import './index.css';
 import App from './App';
 
+const router = createBrowserRouter([
+	{
+		path: '/',
+		element: <App />
+	}
+]);
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
-		<BrowserRouter>
-			<App />
-		</BrowserRouter>
+		<RouterProvider router={router} />
 	</React.StrictMode>
 );
